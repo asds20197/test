3d5106ad962d175409ad9095f75c6e4a9512f5eb
asds20197 <35724123+asds20197@users.noreply.github.com>
2018-01-26 09:47:33 +0800

    2.java
    test

---
+++ app2_2.java
@@ -0,0 +1,14 @@
+// app2_2, 本體與區塊
+public class app2_2   // 定義public類別app2_2
+{
+   public static void main(String args[])  
+   {
+      int i;
+      i=5;
+      if(i<8)
+      {
+         System.out.println("變數i的值小於8"); // 印出字串內容
+         System.out.println("i="+i);            // 印出變數
+      }
+   }
+}
